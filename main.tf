4f514e404bdc7f47eb87db059db1a48e0d41ee4b
José Câmara <98744315+JoseMigCamara@users.noreply.github.com>
2022-04-28 10:04:41 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,133 @@
+resource "aws_eip" "eip0" {
+  network_border_group = "us-east-1"
+  tags                 = {}
+  vpc                  = true
+
+  timeouts {}
+}
+
+# aws_instance.instance:
+resource "aws_instance" "instance" {
+    ami                                  = "ami-0f9fc25dd2506cf6d"
+    availability_zone                    = "us-east-1a"
+    instance_type                        = "t2.micro"
+    key_name                             = "GRSI"
+    security_groups                      = [
+        aws_security_group.sg0.name,
+    ]
+    vpc_security_group_ids               = [
+        aws_security_group.sg0.id,
+    ]
+    
+    ebs_block_device {
+    	delete_on_termination = true
+    	device_name           = "/dev/sdb"
+    	volume_size           = 12
+    	volume_type           = "gp3"
+    }
+    
+    root_block_device {
+        delete_on_termination = true
+        encrypted             = false
+        volume_size           = 8
+        volume_type           = "gp2"
+    }
+}
+
+# aws_eip_association.eip0_assoc:
+resource "aws_eip_association" "eip0_assoc" {
+  allocation_id        = aws_eip.eip0.id
+instance_id          = aws_instance.instance.id
+}
+
+# aws_security_group.sg0:
+resource "aws_security_group" "sg0" {
+    description = "GRSI"
+    egress      = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+    ingress     = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = "Permitir todo o iCMP"
+            from_port        = -1
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "icmp"
+            security_groups  = []
+            self             = false
+            to_port          = -1
+        },
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 22
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 22
+        },
+        {
+            cidr_blocks      = [
+                "78.29.147.32/32",
+            ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "udp"
+            security_groups  = []
+            self             = false
+            to_port          = 65535
+        },
+        {
+            cidr_blocks      = [
+                "78.29.147.32/32",
+            ]
+            description      = ""
+            from_port        = 443
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 443
+        },
+        {
+            cidr_blocks      = [
+                "78.29.147.32/32",
+            ]
+            description      = ""
+            from_port        = 80
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 80
+        },
+    ]
+    name        = "grsi-security"  
+    tags        = {}
+    tags_all    = {}
+
+    timeouts {}
+}
