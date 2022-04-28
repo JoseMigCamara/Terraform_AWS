ca9ca5258369b7cd197d3a8c9d31da4473fcf092
José Câmara <98744315+JoseMigCamara@users.noreply.github.com>
2022-04-28 10:17:41 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,74 +58,61 @@ resource "aws_security_group" "sg0" {
             self             = false
             to_port          = 0
         },
-    ]
-    ingress     = [
-        {
-            cidr_blocks      = [
-                "0.0.0.0/0",
-            ]
-            description      = "Permitir todo o iCMP"
-            from_port        = -1
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "icmp"
-            security_groups  = []
-            self             = false
-            to_port          = -1
-        },
-        {
-            cidr_blocks      = [
-                "0.0.0.0/0",
-            ]
-            description      = ""
-            from_port        = 22
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "tcp"
-            security_groups  = []
-            self             = false
-            to_port          = 22
-        },
-        {
-            cidr_blocks      = [
-                "78.29.147.32/32",
-            ]
-            description      = ""
-            from_port        = 0
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "udp"
-            security_groups  = []
-            self             = false
-            to_port          = 65535
-        },
-        {
-            cidr_blocks      = [
-                "78.29.147.32/32",
-            ]
-            description      = ""
-            from_port        = 443
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "tcp"
-            security_groups  = []
-            self             = false
-            to_port          = 443
-        },
-        {
-            cidr_blocks      = [
-                "78.29.147.32/32",
-            ]
-            description      = ""
-            from_port        = 80
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "tcp"
-            security_groups  = []
-            self             = false
-            to_port          = 80
-        },
-    ]
+  ]
+  ingress     = [
+    {
+      cidr_blocks      = [
+        "0.0.0.0/0",
+      ]
+      description      = "Permitir todo o iCMP"
+      from_port        = -1
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "icmp"
+      security_groups  = []
+      self             = false
+      to_port          = -1
+    },
+    {
+      cidr_blocks      = [
+        "78.29.147.32/32",
+      ]
+      description      = "Permitir todo o DNS vindo do meu IP"
+      from_port        = 53
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "udp"
+      security_groups  = []
+      self             = false
+      to_port          = 53
+    },
+    {
+      cidr_blocks      = [
+        "78.29.147.32/32",
+      ]
+      description      = "Permitir todo o HTTPS vindo do meu IP"
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+    },
+    {
+      cidr_blocks      = []
+      description      = "Permitir todo o HTTP vindo de IPv6"
+      from_port        = 80
+      ipv6_cidr_blocks = [
+        "::/0",
+      ]
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    },
+  ]
     name        = "grsi-security"  
     tags        = {}
     tags_all    = {}
